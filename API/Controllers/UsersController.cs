1b187ca78b7bc02eb3da2e2812b987b0fa239e46
baljeetkis <baljeetc.kis@gmail.com>
2023-09-26 15:18:31 +0530

    Updated code

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ public class UsersController : ControllerBase
         return users;
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}")] //
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         return await _context.Users.FindAsync(id);
